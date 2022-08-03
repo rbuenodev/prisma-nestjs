d62df78899ab6090c0fc055804b20a5487426d61
Richard <richard_bueno@ymail.com>
2022-08-02 23:18:15 -0300

    common filters

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
+import { UseFilters, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { filter } from 'rxjs';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT || 3306);
 }
 bootstrap();
